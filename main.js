658649c98e71841161e0942f76e63355fafd604a
Willians-Hurtado <willianshurtado5@gmail.com>
2025-02-18 15:07:59 +0100

    add button that turns on the lamp

---
+++ main.js
@@ -0,0 +1,33 @@
+const lightButton = document.querySelector('button')
+
+lightButton.addEventListener(`click`, function () {
+    console.log('clickable botton');
+    const picEl = document.getElementById('pic');
+    picEl.src = '../img/yellow_lamp.png'
+   
+})
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
